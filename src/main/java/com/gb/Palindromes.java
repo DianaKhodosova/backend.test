2b739428eb3587622f285731dfb6675ae645ab42
Diana Khodosova <diana-17@yandex.ru>
2022-09-05 18:18:54 +0300

    Backend.test

---
+++ src/main/java/com/gb/Palindromes.java
@@ -0,0 +1,30 @@
+package com.gb;
+
+
+import java.util.Locale;
+
+public class Palindromes {
+    public static void main(String[] args) {
+         String original = "A roza upala na Lapu Azora";
+         original = original.replace(" ", "");
+         original = original.toLowerCase(Locale.forLanguageTag("[^a-zA-Z0-9]"));
+
+         String reverse = "";
+         for (int i = original.length() - 1; i >= 0; i--) {
+             reverse += original.charAt(i);
+             System.out.println(reverse);
+
+         }
+         boolean palindrome = true;
+         for (int i = 0; i < original.length(); i++) {
+             if (original.charAt(i) != reverse.charAt(i)) {
+                 palindrome = false;
+             }
+         }
+         if (palindrome) {
+             System.out.println("PALINDROME!");
+         } else {
+             System.out.println("Not a palindrome!");
+         }
+    }
+}
---
+++ src/test/java/com/gb/ValidTest.java
@@ -0,0 +1,19 @@
+package com.gb;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+public class ValidTest {
+
+    @Test
+    public  void test() {
+        Palindromes solution = new Palindromes();
+        String str1 = "A roza upala na Lapu Azora";
+        //Assertions.assertTrue(solution.palindrome(str1));
+
+        //тут я запуталась, стала пересматривать урок и потом полезла в интернет,
+        // чтобы там посмотреть как писать эти тесты. И всё поплыла....
+    }
+
+}
