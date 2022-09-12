e61742e5c500f1c277f7415462eba7f6c3499543
Diana Khodosova <diana-17@yandex.ru>
2022-09-12 18:36:31 +0300

    дз5

---
+++ src/main/java/com/gb/RetrofitUtils.java
@@ -0,0 +1,27 @@
+package com.gb;
+
+import lombok.experimental.UtilityClass;
+import lombok.extern.slf4j.Slf4j;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+@UtilityClass
+@Slf4j
+public class RetrofitUtils {
+
+    HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+    OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
+
+    public Retrofit getRetrofit() {
+
+        logging.setLevel(HttpLoggingInterceptor.Level.BODY);
+        httpClient.addInterceptor(logging);
+        return new Retrofit.Builder()
+                .baseUrl("https://minimarket1.herokuapp.com/market/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(httpClient.build())
+                .build();
+    }
+}
